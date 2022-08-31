53c31770d982d42d70fdc61f27c20cef444de010
Raphael Berger <raphaelmb@gmail.com>
2022-08-31 15:16:17 -0300

    Add event bus

--- lab/comments-service/index.js
+++ lab/comments-service/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { randomUUID } = require("crypto");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -12,12 +13,24 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomUUID();
   const { content } = req.body;
   const comments = commentsByPostId[req.params.id] || [];
   comments.push({ id: commentId, content });
   commentsByPostId[req.params.id] = comments;
+  try {
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentCreated",
+      data: {
+        id: commentId,
+        content,
+        postId: req.params.id,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
   res.status(201).send(comments);
 });
 
