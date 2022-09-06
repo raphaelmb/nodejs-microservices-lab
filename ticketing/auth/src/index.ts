926ec368b8d93a8fe77a227fea176745efdca9bf
Raphael Berger <raphaelmb@gmail.com>
2022-09-06 14:57:59 -0300

    Project setup

---
+++ ticketing/auth/src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("Hi");
+});
+
+app.listen(3000, () => console.log("[Auth] Listening on port 3000"));
