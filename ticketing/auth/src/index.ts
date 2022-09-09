4ea3af67a23ac5406dc34fd976ec4f48c660a5a3
Raphael Berger <raphaelmb@gmail.com>
2022-09-09 17:58:24 -0300

    Add database connection

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import "express-async-errors";
+import mongoose from "mongoose";
 import { errorHandler } from "./middlewares/error-handler";
 import { router } from "./routes";
 
@@ -9,4 +10,12 @@ app.use(express.json());
 app.use(router);
 app.use(errorHandler);
 
-app.listen(3000, () => console.log("[Auth] Listening on port 3000"));
+(async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to DB");
+  } catch (error) {
+    console.error(error);
+  }
+  app.listen(3000, () => console.log("[Auth] Listening on port 3000"));
+})();
