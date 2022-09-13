912f88d328c53ab1ca805639a3c62de9d57f720f
Raphael Berger <raphaelmb@gmail.com>
2022-09-13 19:02:57 -0300

    Add jwt auth

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,11 +1,19 @@
 import express from "express";
 import "express-async-errors";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 import { errorHandler } from "./middlewares/error-handler";
 import { router } from "./routes";
 
 const app = express();
+app.set("trust proxy", true);
 app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(router);
 app.use(errorHandler);
