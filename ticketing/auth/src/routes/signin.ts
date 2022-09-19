9a2a1524e3e87e2e6ab4761a809dae94069c8c56
Raphael Berger <raphaelmb@gmail.com>
2022-09-19 18:20:47 -0300

    Add signin logic

--- ticketing/auth/src/routes/signin.ts
+++ ticketing/auth/src/routes/signin.ts
@@ -1,9 +1,48 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
+import { body } from "express-validator";
+import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest } from "../middlewares/validate-requests";
+import { User } from "../models/user";
+import { Password } from "../services/password";
+import jwt from "jsonwebtoken";
 
 const router = Router();
 
-router.post("/", (req, res) => {
-  res.send("Hi signin!");
-});
+router.post(
+  "/",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("You must supply a password"),
+  ],
+  validateRequest,
+  async (request: Request, response: Response) => {
+    const { email, password } = request.body;
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("Invalid credentials");
+    }
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+    if (!passwordsMatch) {
+      throw new BadRequestError("Invalid credentials");
+    }
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+    request.session = {
+      jwt: userJwt,
+    };
+    return response.status(200).send(existingUser);
+  }
+);
 
 export { router as signinRouter };
--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,8 +1,8 @@
 import { Response, Request, Router } from "express";
-import { body, validationResult } from "express-validator";
+import { body } from "express-validator";
 import jwt from "jsonwebtoken";
 import { BadRequestError } from "../errors/bad-request-error";
-import { RequestValidationError } from "../errors/request-validation-error";
+import { validateRequest } from "../middlewares/validate-requests";
 import { User } from "../models/user";
 
 const router = Router();
@@ -16,11 +16,8 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 characters"),
   ],
+  validateRequest,
   async (request: Request, response: Response) => {
-    const errors = validationResult(request);
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
     const { email, password } = request.body;
 
     const existingUser = await User.findOne({ email });
