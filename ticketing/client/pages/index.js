0fca8b8709a4c46fa65365ec8f4db86d5adad691
Raphael Berger <raphaelmb@gmail.com>
2022-09-26 16:58:50 -0300

    Add request wrapper

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,3 +1,14 @@
-export default () => {
+import axios from "axios";
+
+const LadingPage = ({ currentUser }) => {
   return <h1>Landing page</h1>;
 };
+
+LadingPage.getInitialProps = async () => {
+  const response = await axios.get("/api/users/currentuser").catch((err) => {
+    console.log(err.message);
+  });
+  return response.data;
+};
+
+export default LadingPage;
